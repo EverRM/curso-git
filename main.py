d82b4b8b218a989f0e073a7aa687458d286da9ff
EverRM <113306640+EverRM@users.noreply.github.com>
2022-09-16 21:45:26 -0500

    bucle agregado
    while agregado

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #Tivia 
 
 #declarando librerias
+from pickle import TRUE
 import random
 import time
 
@@ -11,7 +12,7 @@ rojo="\033[31m"
 reset="\033[39m"
 
   
-puntaje=random.randint(0, 20)
+
 participante=input("Bienvenido participante, como podemos nombrarte: ")
 print(" *************************************************")
 print(participante +", Vamos a poner a prueba tus conocimientos")
@@ -19,146 +20,161 @@ print(participante +", Vamos a poner a prueba tus conocimientos")
 print("sobre los componentes de una Pc\n")
 print("selecciona tu respuesta escribiendo la letra de su alternativa y precionando ENTER para validar su respuesta\n")
 print("Acumularas puntaje por cada respuesta correcta, "+"suerte.")
-print("tu puntaje inicial es: ",puntaje)
-print("---------------------------------- \n")
-print("\n*******CARGANDO************\n") 
 
-time.sleep(3)
+while True:
+  puntaje=random.randint(0, 20)
+  print("tu puntaje inicial es: ",puntaje)
+  print("---------------------------------- \n")
+  print("\n*******CARGANDO************\n") 
 
-#primera pregunta 
+  time.sleep(3)
 
-print("1-. Cual es el componente mas importante de una computadora\n")
-print("A) memoria Ram")
-print("B) Disco Duro")
-print("C) Tarjeta Grafica")
-print("D) CPU")
+  #primera pregunta 
 
-
-rta1=input("Escriba su respusta: ")
-
-while rta1 not in("a","b","c","d","A","B","C","D"):
- rta1=input("\n*****************\nDebe insertar alguna de las alternativas\n*****************\n \nIntente Nuevamente: ")
-
-
-if rta1 == "d":
-  print(azul,"\nCorrecto ", reset)
-  puntaje=puntaje+5
-
- 
-elif rta1 =="D":
-  print(azul,"\nCorrecto ", reset)
-  puntaje=puntaje+5
   
+  print("1-. Cual es el componente mas importante de una computadora\n")
+  print("A) memoria Ram")
+  print("B) Disco Duro")
+  print("C) Tarjeta Grafica")
+  print("D) CPU")
 
-else: 
-  print(rojo,"\nIncorrecto, descontaremos algunos puntos",reset)
+
+  rta1=input("Escriba su respusta: ")
+
+  while rta1 not in("a","b","c","d","A","B","C","D"):
+    rta1=input("\n*****************\nDebe insertar alguna de las alternativas\n*****************\n \nIntente Nuevamente: ")
+
+
+  if rta1 == "d":
+    print(azul,"\nCorrecto ", reset)
+    puntaje=puntaje+5
+
+  
+  elif rta1 =="D":
+    print(azul,"\nCorrecto ", reset)
+    puntaje=puntaje+5
+    
+
+  else: 
+    print(rojo,"\nIncorrecto, descontaremos algunos puntos",reset)
+    puntaje=puntaje-3
+  print("\n---------------------------------- \n")
+  #segunda pregunta
+
+  print("2-. componente electrónico que sirve para suministrar electricidad al computador\n")
+
+  print("A)  Fuente de poder ")
+  print("B)  memoria Ram")
+  print("C)  torre")
+  print("D)  mouse")
+
+
+
+  rta2=input("Escriba su respusta: ")
+
+  while rta2 not in("a","b","c","d","A","B","C","D"):
+    rta2=input("\n*****************\nDebe insertar alguna de las alternativas\n*****************\n \nIntente Nuevamente: ")
+
+  if rta2 == "a":
+    print(azul,"\nCorrecto ", reset)
+    puntaje=puntaje+5
+
+  elif rta2 =="A":
+    print(azul,"\nCorrecto ", reset)
+    puntaje=puntaje+5
+
+  else: 
+    print(rojo,"\nIncorrecto, descontaremos algunos puntos",reset)
+  puntaje=puntaje-2
+  print("\n---------------------------------- \n")
+
+  #tercera pregunta
+
+  print("3-. De los siguientes, cual es un dispositivo de salida.\n")
+
+  print("A)  Mouse ")
+  print("B)  Monitor")
+  print("C)  Microfono")
+  print("D)  Scaner")
+
+
+
+  rta3=input("Escriba su respusta: ")
+
+
+
+  while rta3 not in("a","b","c","d","A","B","C","D"):
+    rta3=input("\n*****************\nDebe insertar alguna de las alternativas\n*****************\n \nIntente Nuevamente: ")
+
+  if rta3 == "b":
+    print(azul,"\nCorrecto ", reset)
+    puntaje=puntaje+5
+
+  elif rta3 =="B":
+    print(azul,"\nCorrecto ", reset)
+    puntaje=puntaje+5
+
+  else: 
+    print(rojo,"\nIncorrecto, descontaremos algunos puntos",reset)
   puntaje=puntaje-3
-print("\n---------------------------------- \n")
-#segunda pregunta
+  print("\n---------------------------------- \n")
 
-print("2-. componente electrónico que sirve para suministrar electricidad al computador\n")
+  #cuarta pregunta
 
-print("A)  Fuente de poder ")
-print("B)  memoria Ram")
-print("C)  torre")
-print("D)  mouse")
+  print("4-. Son todos los componentes y dispositivos físicos de un computador.\n")
 
-
-
-rta2=input("Escriba su respusta: ")
-
-while rta2 not in("a","b","c","d","A","B","C","D"):
- rta2=input("\n*****************\nDebe insertar alguna de las alternativas\n*****************\n \nIntente Nuevamente: ")
-
-if rta2 == "a":
-  print(azul,"\nCorrecto ", reset)
-  puntaje=puntaje+5
-
-elif rta2 =="A":
-  print(azul,"\nCorrecto ", reset)
-  puntaje=puntaje+5
-
-else: 
-  print(rojo,"\nIncorrecto, descontaremos algunos puntos",reset)
-puntaje=puntaje-2
-print("\n---------------------------------- \n")
-
-#tercera pregunta
-
-print("3-. De los siguientes, cual es un dispositivo de salida.\n")
-
-print("A)  Mouse ")
-print("B)  Monitor")
-print("C)  Microfono")
-print("D)  Scaner")
+  print("A)  Dispositivos de almacenamiento  ")
+  print("B)  Dispositivos de entrada")
+  print("C)  Hardware")
+  print("D)  Software")
 
 
 
-rta3=input("Escriba su respusta: ")
+  rta4=input("Escriba su respusta: ")
 
 
 
-while rta3 not in("a","b","c","d","A","B","C","D"):
- rta3=input("\n*****************\nDebe insertar alguna de las alternativas\n*****************\n \nIntente Nuevamente: ")
-
-if rta3 == "b":
-  print(azul,"\nCorrecto ", reset)
-  puntaje=puntaje+5
-
-elif rta3 =="B":
-  print(azul,"\nCorrecto ", reset)
-  puntaje=puntaje+5
-
-else: 
-  print(rojo,"\nIncorrecto, descontaremos algunos puntos",reset)
-puntaje=puntaje-3
-print("\n---------------------------------- \n")
-
-#cuarta pregunta
-
-print("4-. Son todos los componentes y dispositivos físicos de un computador.\n")
-
-print("A)  Dispositivos de almacenamiento  ")
-print("B)  Dispositivos de entrada")
-print("C)  Hardware")
-print("D)  Software")
+  while rta4 not in("a","b","c","d","A","B","C","D"):
+    rta4=input("\n*****************\nDebe insertar alguna de las alternativas\n*****************\n \nIntente Nuevamente: ")
 
 
+  if rta4 == "c":
+    print(azul,"\nCorrecto ", reset)
+    puntaje=puntaje+5
+    
 
-rta4=input("Escriba su respusta: ")
+    
 
+  elif rta4 =="C":
+    print(azul,"\nCorrecto ", reset)
+    puntaje=puntaje+5
+    
 
+  else: 
+    print(rojo,"\nIncorrecto, descontaremos algunos puntos",reset)
+  puntaje=puntaje-1
 
-while rta4 not in("a","b","c","d","A","B","C","D"):
- rta4=input("\n*****************\nDebe insertar alguna de las alternativas\n*****************\n \nIntente Nuevamente: ")
+  print("\n*******************\n") 
 
+  print("\n*******PROCESANDO************\n") 
 
-if rta4 == "c":
-  print(azul,"\nCorrecto ", reset)
-  puntaje=puntaje+5
+  time.sleep(3)
+  
   
 
-   
+  print("gracias por participar",participante,"tu puntaje total es de", puntaje, "puntos\n")
 
-elif rta4 =="C":
-  print(azul,"\nCorrecto ", reset)
-  puntaje=puntaje+5
+  print("* * * * * * * * * * * * *" )
+  print(" * * * * * * * * * * * *")
+    
+
+  rpta5 = input('Deseas volver a jugar? Si o No : ')
+  print("")
   
-
-else: 
-  print(rojo,"\nIncorrecto, descontaremos algunos puntos",reset)
-puntaje=puntaje-1
-
-print("\n*******************\n") 
-
-print("\n*******PROCESANDO************\n") 
-
-time.sleep(3)
+  if rpta5 !="si":
  
-
-print("gracias por participar",participante,"tu puntaje total es de", puntaje, "puntos\n")
-
-print("* * * * * * * * * * * * *" )
-print(" * * * * * * * * * * * *")
+    break  
 print("\n*******Cerrando Trivia************\n") 
-print("\n*******Esperamos su Regreso************\n")          
+time.sleep(1)
+
+print("\n*******Esperamos su Regreso************\n")    
